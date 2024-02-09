0b8279bdf3cfa19cda78284d7ee6888b347e2ed8
Matteo Michitti <153024204+MatteoM23@users.noreply.github.com>
2024-02-09 18:35:14 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -1,19 +1,30 @@
+// physics.js
 import Matter from 'https://cdn.skypack.dev/pin/matter-js@v0.19.0-Our0SQaqYsMskgmyGYb4/mode=imports/optimized/matter-js.js';
 
 export function initPhysics() {
     const engine = Matter.Engine.create();
-    const world = engine.world;
-
     const render = Matter.Render.create({
         element: document.body,
         engine: engine,
         options: {
-            width: 800,
-            height: 600,
-            wireframes: false
+            width: window.innerWidth,
+            height: window.innerHeight,
+            wireframes: false,
+        },
+    });
+
+    // Add floor
+    const floor = Matter.Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 20, {
+        isStatic: true,
+        render: {
+            fillStyle: '#776e65' // Floor color
         }
     });
 
-    return { engine, world, render };
+    Matter.World.add(engine.world, [floor]);
+
+    Matter.Render.run(render);
+    return { engine, render, world: engine.world };
 }
+
 
