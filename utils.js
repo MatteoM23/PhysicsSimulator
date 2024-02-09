206926321525b98085756b86188f1056390fd67d
Matteo Michitti <153024204+MatteoM23@users.noreply.github.com>
2024-02-09 15:52:14 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,11 +1,12 @@
 import * as Matter from 'https://cdn.skypack.dev/pin/matter-js@v0.19.0-Our0SQaqYsMskgmyGYb4/mode=imports/optimized/matter-js.js';
 
-// Access Body and Vertices from the Matter namespace
-const { Body, Vertices } = Matter;
+// Access relevant parts from the Matter namespace
+const { Body, Vertices, Render } = Matter;
 
-// Converts screen coordinates to physics world coordinates, assuming a 1:1 scale for simplicity
-function screenToWorld(x, y) {
-    return { x, y };
+// Converts screen coordinates to physics world coordinates
+function screenToWorld(clientX, clientY, render) {
+    const point = { x: clientX, y: clientY };
+    return Render.screenToWorld(render, point);
 }
 
 // Checks if a point is inside a body, useful for interactive elements
@@ -23,6 +24,7 @@ function adjustMaterialProperties(body, properties) {
     Body.set(body, properties);
 }
 
+
 // Calculates the magnitude of a vector
 function calculateMagnitude(vector) {
     return Math.sqrt(vector.x ** 2 + vector.y ** 2);
@@ -33,14 +35,15 @@ function applyForceTowardsPoint(body, point, strength) {
     const dx = point.x - body.position.x;
     const dy = point.y - body.position.y;
     const distance = calculateMagnitude({ x: dx, y: dy });
+    const forceMagnitude = strength / distance; // Adjust the strength based on distance
     const force = {
-        x: (dx / distance) * strength,
-        y: (dy / distance) * strength,
+        x: (dx / distance) * forceMagnitude,
+        y: (dy / distance) * forceMagnitude,
     };
     Body.applyForce(body, body.position, force);
 }
 
-// Normalizes a vector to point in the same direction but with a magnitude of 1
+// Normalizes a vector to have a magnitude of 1
 function normalizeVector(vector) {
     const magnitude = calculateMagnitude(vector);
     return {
@@ -49,4 +52,11 @@ function normalizeVector(vector) {
     };
 }
 
-export { screenToWorld, isInside, adjustMaterialProperties, getRandomColor, calculateMagnitude, applyForceTowardsPoint, normalizeVector };
+export {
+    screenToWorld,
+    isInside,
+    adjustMaterialProperties,
+    calculateMagnitude,
+    applyForceTowardsPoint,
+    normalizeVector
+};
