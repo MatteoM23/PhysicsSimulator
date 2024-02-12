89d4d4ede0d9a806586e7e6d96c22e4f5f5b8f0a
Matteo Michitti <153024204+MatteoM23@users.noreply.github.com>
2024-02-12 16:06:02 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -15,21 +15,22 @@ export function initPhysics() {
     });
 
     // Add floor and walls with gradient color
-    const ground = Matter.Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 20, { 
+    const wallThickness = 10; // Adjust the thickness of the walls
+    const ground = Matter.Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, wallThickness, { 
         isStatic: true, 
         render: {
             fillStyle: 'linear-gradient(to right, #e0c3fc, #8ec5fc)', // Gradient fill color for floor
             strokeStyle: '#776e65' // Stroke color for floor
         } 
     });
-    const leftWall = Matter.Bodies.rectangle(20, window.innerHeight / 2, 40, window.innerHeight, { 
+    const leftWall = Matter.Bodies.rectangle(wallThickness / 2, window.innerHeight / 2, wallThickness, window.innerHeight, { 
         isStatic: true, 
         render: {
             fillStyle: 'linear-gradient(to bottom, #e0c3fc, #8ec5fc)', // Gradient fill color for left wall
             strokeStyle: '#776e65' // Stroke color for left wall
         } 
     });
-    const rightWall = Matter.Bodies.rectangle(window.innerWidth - 20, window.innerHeight / 2, 40, window.innerHeight, { 
+    const rightWall = Matter.Bodies.rectangle(window.innerWidth - (wallThickness / 2), window.innerHeight / 2, wallThickness, window.innerHeight, { 
         isStatic: true, 
         render: {
             fillStyle: 'linear-gradient(to bottom, #e0c3fc, #8ec5fc)', // Gradient fill color for right wall
@@ -42,6 +43,7 @@ export function initPhysics() {
     Matter.Render.run(render);
     return { engine, render, world: engine.world };
 }
+
 
 
 
