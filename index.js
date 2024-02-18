e6c44c237ab00043400402b1535e8807928729ed
Matteo Michitti <153024204+MatteoM23@users.noreply.github.com>
2024-02-18 14:41:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -105,10 +105,8 @@ function initPhysics() {
 
     // Define a function to add basic environment elements
     function addEnvironment() {
-    const materialSelectorHeight = document.getElementById('materialsContainer').offsetHeight || 0;
-    const featureButtonsHeight = document.querySelector('.feature-buttons').offsetHeight || 0;
     const groundHeight = Math.min(window.innerHeight * 0.1, 100); // Set ground height to 10% of viewport height or maximum 100px
-    const groundY = window.innerHeight - groundHeight / 2 - featureButtonsHeight - materialSelectorHeight - 10; // Adjust the floor position
+    const groundY = window.innerHeight - groundHeight / 2; // Position the floor at the bottom of the viewport
 
     const ground = Matter.Bodies.rectangle(window.innerWidth / 2, groundY, window.innerWidth, groundHeight, {
         isStatic: true,
@@ -128,6 +126,7 @@ function initPhysics() {
     // Add elements to the world
     Matter.World.add(world, [ground, leftWall, rightWall]);
 }
+
 
 
     addEnvironment(); // Add the initial environment
