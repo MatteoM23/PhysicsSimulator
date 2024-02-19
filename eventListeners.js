9e370f8deadb518c805105edea97e61256f19bd6
Matteo Michitti <153024204+MatteoM23@users.noreply.github.com>
2024-02-19 16:01:16 -0500

    Create eventListeners.js

---
+++ eventListeners.js
@@ -0,0 +1,7 @@
+// eventListeners.js
+import { engine, render } from './physicsInit.js';
+import { createBody, handleTeleportationCollision } from './materialManager.js';
+
+export const setupEventListeners = () => {
+    // Define mouse event listeners and other custom event handlers
+};
