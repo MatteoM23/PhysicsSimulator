c8ebce0df9f843d3ea927f6f86d9dffa2ff1d845
Matteo Michitti <153024204+MatteoM23@users.noreply.github.com>
2024-02-19 16:01:31 -0500

    Create materialManager.js

---
+++ materialManager.js
@@ -0,0 +1,15 @@
+// materialManager.js
+import { engine, world, render } from './physicsInit.js';
+import { screenToWorld } from './utils.js';
+
+export const materials = {
+    // Define materials here
+};
+
+export const createBody = (x, y, materialKey) => {
+    // Function to create a body with the given material properties
+};
+
+export const handleTeleportationCollision = (event) => {
+    // Handle teleportation collisions
+};
