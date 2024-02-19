75f1e9dc7ab4ceb466759b18b48145c479474e30
Matteo Michitti <153024204+MatteoM23@users.noreply.github.com>
2024-02-19 17:41:54 -0500

    Update simulationControls.js

--- simulationControls.js
+++ simulationControls.js
@@ -17,11 +17,21 @@ export const clearWorld = () => {
     console.log('Material bodies cleared!');
 };
 
+let gravityReversed = false; // Variable to track gravity state
+
 export const toggleGravity = () => {
-    // Toggle gravity by setting it to -1 if currently 0, and vice versa
-    engine.world.gravity.y = engine.world.gravity.y === 0 ? -1 : 0;
+    if (gravityReversed) {
+        // If gravity is reversed, set it back to normal
+        engine.world.gravity.y = 1; // Or whatever your normal gravity value is
+        gravityReversed = false;
+    } else {
+        // If gravity is normal, reverse it
+        engine.world.gravity.y = -1; // Or whatever value you want to set for reversed gravity
+        gravityReversed = true;
+    }
     console.log('Gravity toggled!');
 };
+
 
 export const materialRain = () => {
     const materials = ['sand', 'water', 'oil', 'rock', 'lava', 'ice', 'rubber', 'steel', 'glass', 'wood', 'antimatter', 'darkMatter', 'neutronium', 'quantumFoam', 'exoticMatter', 'plasmaCrystal', 'voidEssence', 'ether', 'solarFlare', 'cosmicDust', 'magneticField', 'photonGel'];
@@ -29,13 +39,17 @@ export const materialRain = () => {
     // Number of materials to rain
     const numMaterials = 20;
 
+    // Get the canvas dimensions
+    const canvasWidth = engine.render.canvas.width;
+    const canvasHeight = engine.render.canvas.height;
+
     // Create material rain
     for (let i = 0; i < numMaterials; i++) {
         const material = materials[Math.floor(Math.random() * materials.length)];
 
         // Generate random position within the canvas
-        const x = Math.random() * engine.render.canvas.width;
-        const y = Math.random() * engine.render.canvas.height;
+        const x = Math.random() * canvasWidth;
+        const y = Math.random() * canvasHeight;
 
         // Create body at random position with selected material
         createBody(x, y, material);
@@ -43,3 +57,4 @@ export const materialRain = () => {
 
     console.log('Material rain started!');
 };
+
