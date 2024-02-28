79e403094bf36f38e1279f267898e78669e9e666
Matteo Michitti <153024204+MatteoM23@users.noreply.github.com>
2024-02-27 22:09:26 -0500

    Update simulationControls.js

--- simulationControls.js
+++ simulationControls.js
@@ -1,6 +1,6 @@
 import { engine, world } from './physicsInit.js';
 import Matter from 'https://cdn.skypack.dev/matter-js';
-import { createBody, materials } from './materialManager.js'; // Import materials here
+import { materials } from './materialManager.js'; // Import materials here
 import { createBody } from './eventListeners.js'; // Import materials here
 
 let gravityReversed = false; // Variable to track gravity state
